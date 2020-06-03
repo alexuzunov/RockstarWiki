0f867a05e1383acaf62a449f0c8c9b366d9afc12
alexuzunov <alex.uzunov2002@gmail.com>
2020-06-03 14:25:47 +0300

    Added gulpfile and a proper file structure

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const clean = require('gulp-clean-css');
+const postcss = require('gulp-postcss');
+const sasslint = require('gulp-sass-lint');
+
+gulp.task('sass', () => {
+	return gulp.src('./src/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([ autoprefixer() ]))
+		.pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('minify', () => {
+	return gulp.src('./dist/css/*.css')
+		.pipe(clean())
+		.pipe(gulp.dest('./dist/css/minifiles'))
+});
+
+gulp.task('lint', () => {
+	return gulp.src('./src/sass/*.scss')
+		.pipe(sasslint())
+		.pipe(sasslint.format())
+		.pipe(sasslint.failOnError())
+});
+
+gulp.task('default', () => {
+	return gulp.watch('./src/sass/*.scss', gulp.series('sass', 'minify', 'lint'));
+})
